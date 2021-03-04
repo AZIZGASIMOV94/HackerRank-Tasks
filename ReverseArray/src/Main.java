ff46d00ec34191e3c40f1cf7e01a3a4a65f5f411
Aziz Gasimov <aziz.gasimov94@gmail.com>
2021-03-04 11:03:09 +0400

    project: reverse an integer array from hacker rank

---
+++ ReverseArray/src/Main.java
@@ -0,0 +1,23 @@
+/*
+* HackerRank task no:1
+* */
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3};
+        int res[] = reverse(arr);
+        for (int elem : res){
+            System.out.print(elem+" ");
+        }
+    }
+
+    public static int[] reverse(int[] array){
+        int[] newArray = new int[array.length];
+        for (int i = array.length-1; i >= 0; ){
+            for (int j =0; j< newArray.length; j++){
+                newArray[j] = array[i];
+                i--;
+            }
+        }
+        return newArray;
+    }
+}
